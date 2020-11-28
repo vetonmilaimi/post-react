d8f1a233d51777163ef12fedf2c63c2d06c25051
Veton Milaimi <vetonmila@gmail.com>
2020-11-28 23:48:51 +0100

    Remove datetime package because I make the creation data more easy, and done with remove button

--- src/components/PostContent.js
+++ src/components/PostContent.js
@@ -1,16 +1,37 @@
 import React from "react";
 
 class PostContent extends React.Component {
+    state = {
+        date: ""
+    };
+
+    componentDidMount() {
+        this.getDate();
+    }
+
+    getDate = () => {
+        let date = new Date().toLocaleString();
+        this.setState({ date });
+    }
+
+
     render() {
-        const posts = this.props.posts.map((post,i) =>{
+        const { date } = this.state;
+
+        const posts = this.props.posts.map((post, i) => {
             return (
                 <div className="post-content" key={i}>
                     <h3>{post.title}</h3>
                     <p>{post.postText}</p>
-                    <p>Creation Data: {this.date}</p>
+                    <hr />
+                    <p>Creation Data: {date}</p>
+
+                    <button onClick={() => {
+                        document.getElementsByClassName("post-content")[i].remove();
+                    }}>Remove</button>
                 </div>
             );
-        })
+        });
 
         return (
             <div>
